c01f7a5703f1a9900ff4a19ef731a20cbda5bc93
justseydanur <seydanurrcakir@gmail.com>
2025-09-09 12:26:12 +0300

    09.09.2025

---
+++ Infrastructure/HRSystem.Persistence/Migrations/20250909092048_AddUserRoleX.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRSystem.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRoleX : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Employees",
+                newName: "email");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Employees",
+                newName: "Email");
+        }
+    }
+}
